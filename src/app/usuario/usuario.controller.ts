aced7f610a434f3236f5c15349039dfdbffb1138
DanielMachado <danielmachado3fs@gmail.com>
2023-05-10 23:21:16 -0300

    feat(typeORM): adiciciona comandos do typeORM no package.json

--- src/app/usuario/usuario.controller.ts
+++ src/app/usuario/usuario.controller.ts
@@ -1,41 +1,34 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
+  Controller
 } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
-import { CreateUsuarioDto, UpdateUsuarioDto } from './usuario.dto';
 
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
-  @Post()
-  create(@Body() createUsuarioDto: CreateUsuarioDto) {
-    return this.usuarioService.create(createUsuarioDto);
-  }
+  // @Post()
+  // create(@Body() createUsuarioDto: CreateUsuarioDto) {
+  //   return this.usuarioService.criar(createUsuarioDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.usuarioService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.usuarioService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usuarioService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usuarioService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
-    return this.usuarioService.update(+id, updateUsuarioDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+  //   return this.usuarioService.update(+id, updateUsuarioDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usuarioService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usuarioService.remove(+id);
+  // }
 }
