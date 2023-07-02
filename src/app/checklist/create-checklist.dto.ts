78acaca18378f1e648ffa630ca48d66241278e96
DanielMachado <danielmachado3fs@gmail.com>
2023-07-02 10:38:47 -0300

    feat: adiciona dto de checklist

--- src/app/checklist/create-checklist.dto.ts
+++ src/app/checklist/create-checklist.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from "@nestjs/mapped-types";
 
-export class CreateChecklistDto {}
+export class CreateChecklistDto {
+    
+}
 export class UpdateChecklistDto extends PartialType(CreateChecklistDto) {}
